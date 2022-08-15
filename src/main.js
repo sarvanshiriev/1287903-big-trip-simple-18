0a3218967152cbf243fdca12609747b07811048e
sarvanshiriev <sarvan.shiriev@mail.ru>
2022-08-15 18:26:57 +0500

    Время разбивать камни

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Filter from './view/filter';
+import Sort from './view/sort';
 import { render,RenderPosition } from './render';
+import RoutePresenter from './presenter/route-presenter';
 
-const containerPlace = document.querySelector('.trip-controls__filters');
-render (new Filter,containerPlace,RenderPosition.BEFOREEND);
+const containerFilterPlace = document.querySelector('.trip-controls__filters');
+const containerPlace = document.querySelector('.trip-events');
+const routePresenter = new RoutePresenter();
+
+render (new Filter,containerFilterPlace,RenderPosition.BEFOREEND);
+render (new Sort,containerPlace,RenderPosition.BEFOREEND);
+
+routePresenter.itin(containerPlace);
---
+++ src/presenter/route-presenter.js
@@ -0,0 +1,8 @@
+import FormAdd from "../view/form-add";
+import FormEdit from "../view/form-edit";
+import PointRoute from "../view/point-route";
+import { render } from "../render";
+
+export default class RoutePresenter {
+
+}
