e2ff2f578792e0e4f48ab21f79c6253eb1346b2c
sarvanshiriev <sarvan.shiriev@mail.ru>
2022-08-25 18:33:56 +0500

    Шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Filter from './view/filter';
 import Sort from './view/sort';
-import { render,RenderPosition } from './render';
+import { render} from './render';
 import RoutePresenter from './presenter/route-presenter';
 
 const containerFilterPlace = document.querySelector('.trip-controls__filters');
 const containerPlace = document.querySelector('.trip-events');
 const routePresenter = new RoutePresenter();
 
-render (new Filter () , containerFilterPlace , RenderPosition.BEFOREEND);
-render (new Sort () , containerPlace , RenderPosition.BEFOREEND);
+render (new Filter () , containerFilterPlace );
+render (new Sort () , containerPlace );
 
 routePresenter.init(containerPlace);
