491fecec714bc51538e6296dea35df0fe3364aa1
sarvanshiriev <sarvan.shiriev@mail.ru>
2022-10-17 12:14:53 +0500

    Добавим презентер фильтров

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-import Filter from './view/filter';
 import {render} from './framework/render.js';
 import RoutePresenter from './presenter/route-presenter';
 import PointModel from './model/point-model';
 import FilterModel from './model/filter-model.js';
+import FilterPresenter from './presenter/filter-presenter.js';
 
 const containerFilterPlace = document.querySelector('.trip-controls__filters');
 const containerPlace = document.querySelector('.trip-events');
 const pointModel = new PointModel();
-const routePresenter = new RoutePresenter(containerPlace,pointModel);
 const filterModel = new FilterModel();
 
-render (new Filter () , containerFilterPlace);
+const routePresenter = new RoutePresenter(containerPlace, pointModel, filterModel);
+const filterPresenter = new FilterPresenter(containerFilterPlace, filterModel, pointModel);
+
+filterPresenter.init();
 routePresenter.init();
 
